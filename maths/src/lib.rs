e3c72dcf2dacb6de478cb42eef9ce3d34f5258b2
Samuel Owen <SamHirael@gmail.com>
2023-08-23 10:01:21 +0100

    feat: :white_check_mark: add is NaN check

--- maths/src/lib.rs
+++ maths/src/lib.rs
@@ -60,11 +60,19 @@ mod mean_tests {
     }
 }
 
-pub fn median(numbers: &Vec<f64>) -> f64 {
+fn contains_nan(numbers: &Vec<f64>) -> bool {
+    numbers.iter().any(|&x| x.is_nan())
+}
+
+pub fn median(numbers: &Vec<f64>) -> Option<f64> {
+    if contains_nan(numbers) {
+        return None;
+    }
+
     let mut numbers_owned = numbers.to_owned();
     let numbers_length = numbers.len();
     match numbers_length {
-        0 => 0.0,
+        0 => None,
         _ => {
             numbers_owned.sort_by(|a, b| a.partial_cmp(b).unwrap());
             match numbers_length % 2 {
@@ -72,12 +80,12 @@ pub fn median(numbers: &Vec<f64>) -> f64 {
                     // Even Number
                     let big_index = numbers_length / 2;
                     let small_index = (numbers_length / 2) - 1;
-                    (numbers_owned[small_index] + numbers_owned[big_index]) / 2.0
+                    Some((numbers_owned[small_index] + numbers_owned[big_index]) / 2.0)
                 }
                 _ => {
                     // Odd Number:
                     let index = numbers_length / 2;
-                    numbers_owned[index]
+                    Some(numbers_owned[index])
                 }
             }
         }
@@ -86,66 +94,73 @@ pub fn median(numbers: &Vec<f64>) -> f64 {
 
 #[cfg(test)]
 mod median_tests {
+
     use super::*;
 
     #[test]
     fn it_tests_we_have_a_median_function() {
         let result = median(&vec![0.0]);
-        assert_eq!(result, 0.0);
+        assert_eq!(result, Some(0.0));
     }
 
     #[test]
-    fn returns_zero_for_empty_vector() {
+    fn returns_none_for_empty_vector() {
         let result = median(&vec![]);
-        assert_eq!(result, 0.0);
+        assert_eq!(result, None);
     }
 
     #[test]
     fn returns_correct_median_small_vector() {
         let result = median(&vec![1.0, 2.0, 3.0]);
-        assert_eq!(result, 2.0);
+        assert_eq!(result, Some(2.0));
     }
 
     #[test]
     fn returns_correct_median_even_num_elements() {
         let result = median(&vec![2.0, 3.0]);
-        assert_eq!(result, 2.5);
+        assert_eq!(result, Some(2.5));
     }
 
     #[test]
     fn returns_correct_median_unordered_elements() {
         let result = median(&vec![3.0, 1.0, 2.0]);
-        assert_eq!(result, 2.0);
+        assert_eq!(result, Some(2.0));
     }
 
     #[test]
     fn returns_correct_median_negative_numbers() {
         let result = median(&vec![-3.0, -1.0, -2.0]);
-        assert_eq!(result, -2.0);
+        assert_eq!(result, Some(-2.0));
     }
 
     #[test]
     fn returns_correct_median_mixed_numbers() {
         let result = median(&vec![-2.0, 0.0, 2.0]);
-        assert_eq!(result, 0.0);
+        assert_eq!(result, Some(0.0));
     }
 
     #[test]
     fn returns_correct_median_repeating_numbers() {
         let result = median(&vec![1.0, 1.0, 1.0]);
-        assert_eq!(result, 1.0);
+        assert_eq!(result, Some(1.0));
     }
 
     #[test]
     fn returns_correct_median_large_vector() {
         let result = median(&vec![1.0; 1000]);
-        assert_eq!(result, 1.0);
+        assert_eq!(result, Some(1.0));
     }
 
     #[test]
     fn returns_correct_median_single_element() {
         let result = median(&vec![42.0]);
-        assert_eq!(result, 42.0);
+        assert_eq!(result, Some(42.0));
+    }
+
+    #[test]
+    fn it_doesnt_crash_with_nan_input() {
+        let result = median(&vec![42.0, f64::NAN]);
+        assert_eq!(result, None)
     }
 }
 
